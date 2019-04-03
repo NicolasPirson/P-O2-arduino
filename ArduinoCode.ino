01ac71d5d6e634bfde9ce2815bcf692b74092cab
KarelSmets00 <49246728+KarelSmets00@users.noreply.github.com>
2019-04-03 19:58:12 +0200

    Create ArduinoCode.ino

---
+++ ArduinoCode.ino
@@ -0,0 +1,16 @@
+#include <PWMSoft.h>
+#include <SoftwareSerial.h>
+
+int Duwmotor;
+int Versnel_Motor;
+int Servo;
+int Speed_Trigger_1;
+int Speed_Trigger_2;
+
+void setup(){
+
+}
+
+void loop(){
+
+}
