86615880882d491f25f165d6685a13e0ec39b9f3
Nicolas Pirson <48385955+NicolasPirson@users.noreply.github.com>
2019-04-04 17:06:17 +0200

    Update ArduinoCode.ino

--- ArduinoCode.ino
+++ ArduinoCode.ino
@@ -8,9 +8,31 @@ int Speed_Trigger_1;
 int Speed_Trigger_2;
 
 void setup(){
+  pinMode(Servo,OUTPUT);
+  pinMode(Duwmotor,OUTPUT);
+  pinMode(Versnel_Motor,OUTPUT);
+  pinMode(Speed_Trigger_1,INPUT);
+  pinmode(Speed_Trigger_2,INPUT);
 
 }
 
 void loop(){
+  
+}
 
+void SnelheidMeting(){
+  
+  int Lichtniveau1 = digitalRead(Speed_Trigger_1);
+  int Lichtniveau2 = digitalRead(Speed_Trigger_2);
+    
+  //tijd meten: millis();
+  if (Lichtniveau1 == 0){   // 0 moet vervangen worden met waarde die verkregen wordt wanneer pingpongbal voor licht staat
+    start = millis();
+  }
+  if (Lichtniveau2 == 0){   
+    finish = millis();
+    
+  }
+  verlopen_tijd = finish-start
+  snelheid = 0.04/verlopen_tijd*(pow(10,-3)) // met afstand = de afstand tussen de 2 gaten = 4cm
 }
